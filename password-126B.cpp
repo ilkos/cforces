fce3c0ea3fb899030517fc3c0bd2bdfa29f01fe9
Ioannis Ilkos <ioannis.ilkos@gmail.com>
2015-09-16 23:47:23 +0100

    Constant time rabin karp hashing for substrings

--- password-126B.cpp
+++ password-126B.cpp
@@ -18,40 +18,32 @@ bool equalStr(const string& s1, const int lo1, const string& s2, const int lo2, 
     return true;
 }
 
-int64_t powMod(int64_t num, int32_t power, int64_t modulo) {
-    int64_t result = 1;
-    for (int i = 0; i < power; ++i) {
-        result = (result * num) % modulo;
+int64_t rkhash(const vector<int64_t>& primePowers, const vector<int64_t>& partialHashes, int lo, int hi) {
+    int64_t result = partialHashes[hi];
+    if (lo > 0) {
+        result -= partialHashes[lo - 1];
+        result %= LARGE_PRIME;
+        if (result < 0) result += LARGE_PRIME;
     }
-    return result;
+
+    result *= primePowers[primePowers.size() - 1 - lo];
+    return result % LARGE_PRIME;
 }
 
-int rkmatch(const string& s, int len) {
+int rkmatch(const vector<int64_t>& primePowers, const vector<int64_t>& partialHashes,
+            const string& s, int len) {
     if (!len) return -1;
 
     // matches s[0, len) with another substring of s[]
-    int64_t patternHash = 0;
-    for (int i = 0; i < len; ++i) {
-        patternHash *= PRIME;
-        patternHash += s[i];
-        patternHash %= LARGE_PRIME;
-    }
+    const int64_t searchedHash = rkhash(primePowers, partialHashes, 0, len - 1);
 
-    const int64_t searchedHash = patternHash;
-    const int64_t lenPow = powMod(PRIME, len - 1, LARGE_PRIME);
-
-    for (int i = len; i < s.size(); ++i) {
-        patternHash -= s[i - len] * lenPow;
-        patternHash *= PRIME;
-        patternHash += s[i];
-        patternHash %= LARGE_PRIME;
-
-        if (patternHash < 0) patternHash += LARGE_PRIME;
+    for (int i = 1; i + len < s.size(); ++i) {
+        const int64_t patternHash = rkhash(primePowers, partialHashes, i, i + len - 1);
 
         if (patternHash == searchedHash) {
             // actual string comparison
-            if (equalStr(s, 0, s, i - len + 1, len)) {
-                return i - len + 1;
+            if (equalStr(s, 0, s, i, len)) {
+                return i;
             }
         }
     }
@@ -60,9 +52,27 @@ int rkmatch(const string& s, int len) {
 }
 
 string solve(const string& s) {
+    // precalculate the powers of our prime
+    auto primePowers = vector<int64_t>(s.size());
+    primePowers[s.size() - 1] = 1;
+    for (int i = s.size() - 2; i >= 0; --i) {
+        primePowers[i] = (PRIME * primePowers[i + 1]) % LARGE_PRIME;
+    }
+
+    // precalculate the partial hashes of s
+    auto partialHashes = vector<int64_t>(s.size());
+    for (int i = 0; i < s.size(); ++i) {
+        partialHashes[i] = (primePowers[i] * s[i]) + ((i > 0) ? partialHashes[i - 1] : 0);
+        partialHashes[i] %= LARGE_PRIME;
+    }
+
+    // at this point partialHashes[s.size() - 1] is the hash of the whole string
+    // our hash reference is primePowers[0]
+    // (for all hashes calculated, this should be the multiplier of the 1st char)
+
     // initially find the largest prefix that is also a match for another substring
     // if there exist one of length l, one of l - 1 is also bound to exist -> binary search
-    int lo = 0;
+    int lo = 1;
     int hi = s.size() - 1;
 
     int highestLen = 0;
@@ -70,7 +80,7 @@ string solve(const string& s) {
     while (lo <= hi) {
         int mid = lo + (hi - lo) / 2;
 
-        int match = rkmatch(s, mid);
+        int match = rkmatch(primePowers, partialHashes, s, mid);
 
         if (match >= 0 && match + mid < s.size()) {
             lo = mid + 1;
@@ -88,8 +98,12 @@ string solve(const string& s) {
     }
 
     while (highestLen > 0) {
-        bool prefixSuffixMatch = equalStr(s, 0, s, s.size() - highestLen, highestLen);
-        if (prefixSuffixMatch) break;
+        if (rkhash(primePowers, partialHashes, 0, highestLen - 1) ==
+                rkhash(primePowers, partialHashes, s.size() - highestLen, s.size() - 1)) {
+            bool prefixSuffixMatch = equalStr(s, 0, s, s.size() - highestLen, highestLen);
+            if (prefixSuffixMatch) break;
+        }
+
         --highestLen;
     }
 
