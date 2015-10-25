0a88151bd7fb0405a47951c4067c6c2d424c9c32
Ioannis Ilkos <ioannis.ilkos@gmail.com>
2015-10-25 21:10:56 +0000

    Changed complexity to O(n^2*m)

--- hackerrank/mr_k_marsh.cpp
+++ hackerrank/mr_k_marsh.cpp
@@ -40,34 +40,31 @@ int solve(const vector<string>& field) {
     populateVertical(field, verticalLand);
     
     int best = 0;
+    // fix the top and bottom rows
+    // scanline left to right (with 2 pointers)
     for (int i = 0; i < field.size(); ++i) {
-        for (int j = 0; j < field[0].size(); ++j) {
-            for (int k = i + 1; k < field.size(); ++k) {
-                for (int l = j + 1; l < field[0].size(); ++l) {
-                    // (i,j), (k,l) are the two points
-                    // constant time check to see if we can do a rectangle
-                    
-                    // top
-                    if (l - j > horizontalLand[i][l]) {
-                        continue;
-                    }
-                    
-                    // bottom
-                    if (l - j > horizontalLand[k][l]) {
-                        continue;
-                    }
-                    
-                    // left
-                    if (k - i > verticalLand[k][j]) {
-                        continue;
-                    }
-                    
-                    // right
-                    if (k - i > verticalLand[k][l]) {
+        for (int j = i + 1; j < field.size(); ++j) {
+            int left = 0;
+            int right = 0;
+
+            // find valid left
+            for (; left < field[0].size(); ++left) {
+                if (j - i > verticalLand[j][left]) {
+                    continue;
+                }
+
+                // valid left
+                right = max(right, left + 1);
+                for (; right < field[0].size(); ++right) {
+                    if (j - i > verticalLand[j][right]) {
                         continue;
                     }
 
-                    best = max(best, 2 * (l - j) + 2 * (k - i));
+                    if (right - left <= horizontalLand[i][right] && right - left <= horizontalLand[j][right]) {
+                        best = max(best, 2 * (j - i) + 2 * (right - left));
+                    } else {
+                        break;
+                    }
                 }
             }
         }
