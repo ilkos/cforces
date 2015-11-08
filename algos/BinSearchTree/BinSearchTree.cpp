d206139f0b55fa6d23fbcf1e8ec212d3c579bd61
Ioannis Ilkos <ioannis.ilkos@gmail.com>
2015-11-08 14:28:09 +0000

    Deletion

--- algos/BinSearchTree/BinSearchTree.cpp
+++ algos/BinSearchTree/BinSearchTree.cpp
@@ -1,4 +1,4 @@
-
+#include <vector>
 #include <stddef.h>
 #include <exception>
 #include <cassert>
@@ -20,23 +20,53 @@ struct Node {
     Node(int key, int val) : left(NULL), right(NULL), key(key), val(val) {}
     ~Node() {
         delete left;
-        left = NULL;
         delete right;
-        right = NULL;
     }
 };
 
 class BinSearchTree {
     Node* root;
 
-    static Node* findMin(Node* node) {
+    static Node* findMin(Node* node, Node** parent = 0) {
         if (!node) return NULL;
 
         while (node->left) {
+            if (parent) *parent = node;
             node = node->left;
         }
 
         return node;
+    }
+
+    static Node* findKey(Node* root, int key, Node** parent = 0) {
+        if (parent) *parent = NULL;
+
+        Node* curr = root;
+        while (curr) {
+            if (curr->key == key) {
+                return curr;
+            } else if (key < curr->key) {
+                if (parent) *parent = curr;
+                curr = curr->left;
+            } else {
+                if (parent) *parent = curr;
+                curr = curr->right;
+            }
+        }
+
+        return NULL;
+    }
+
+    Node** getLeadingPtr(Node* parent, Node* child) {
+        if (parent == NULL) {
+            return &root;
+        }
+
+        if (parent->left == child) {
+            return &(parent->left);
+        } else {
+            return &(parent->right);
+        }
     }
 
 public:
@@ -87,18 +117,12 @@ public:
     BinSearchTree() : root(NULL) {}
 
     int get(const int key) const {
-        Node* curr = root;
-        while (curr) {
-            if (curr->key == key) {
-                return curr->val;
-            } else if (key < curr->key) {
-                curr = curr->left;
-            } else {
-                curr = curr->right;
-            }
-        }
+        Node* node = findKey(root, key);
 
-        throw NotFoundException();
+        if (!node)
+            throw NotFoundException();
+
+        return node->key;
     }
 
     void put(const int key, const int value) {
@@ -130,13 +154,66 @@ public:
         }
     }
 
+    void erase(int key) {
+        Node* parent;
+        Node* node = findKey(root, key, &parent);
+        if (!node) {
+            return;
+        }
+
+        Node** leadingPtr = getLeadingPtr(parent, node);
+
+        // is leaf?
+        if (!node->left && !node->right) {
+            delete node;
+            *leadingPtr = NULL;
+            return;
+        }
+
+        // has single child
+        if (node->left && !node->right) {
+            *leadingPtr = node->left;
+            delete node;
+            return;
+        }
+
+        if (node->right && !node->left) {
+            *leadingPtr = node->right;
+            delete node;
+            return;
+        }
+
+        // has both children
+        // replace with the inorder successor
+        Node* successorParent = node;
+        Node* inorderSuccessor = findMin(node->right, &successorParent);
+        // inorder successor might still have a right ptr
+        *getLeadingPtr(successorParent, inorderSuccessor) = inorderSuccessor->right;
+        
+        *leadingPtr = inorderSuccessor;
+        inorderSuccessor->left = node->left;
+        inorderSuccessor->right = node->right;
+
+        node->left = NULL;
+        node->right = NULL;
+        delete node;
+    }
+
+    std::vector<int> keys() {
+        std::vector<int> result;
+        BinSearchTree::iterator iter = begin();
+        while (iter.hasNext()) {
+            result.push_back(iter.next());
+        }
+        return result;
+    }
+
     iterator begin() {
         return iterator(&root);
     }
 
     ~BinSearchTree() {
         delete root;
-        root = NULL;
     }
 };
 
@@ -144,6 +221,18 @@ public:
 void assertEquals(int expected, int value) {
     if (expected != value) {
         std::cout << "Expected: " << expected << " - actual: " << value << std::endl;
+    }
+}
+
+void assertEquals(const std::vector<int>& expected, const std::vector<int>& value) {
+    if (expected != value) {
+        std::cout << "Expected: ";
+        for (int num: expected) std::cout << num << " ";
+        std::cout << std::endl;
+
+        std::cout << "Actual:   ";
+        for (int num: value) std::cout << num << " ";
+        std::cout << std::endl;
     }
 }
 
@@ -164,15 +253,18 @@ int main() {
     tree.put(2, 2);
     tree.put(0, 0);
     tree.put(-5, -5);
+    tree.put(3, 3);
 
     assertEquals(1, tree.get(1));
     assertEquals(2, tree.get(2));
     assertEquals(0, tree.get(0));
     assertThrows([&tree] { tree.get(100); });
 
-    BinSearchTree::iterator iter = tree.begin();
-    while (iter.hasNext()) {
-        std::cout << iter.next() << std::endl;
-    }
+    assertEquals(vector<int>({-5, 0, 1, 2, 3}), tree.keys());
+
+    tree.erase(-5);
+    assertEquals(vector<int>({0, 1, 2, 3}), tree.keys());
+    tree.erase(1);
+    assertEquals(vector<int>({0, 2, 3}), tree.keys());
 }
 #endif
